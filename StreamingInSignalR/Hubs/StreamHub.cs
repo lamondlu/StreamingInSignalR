47f145805dc2657d83f4c90fa52883b95ffc9f38
Nan Lu <lamondlu@firstec.co.nz>
2018-10-23 21:20:22 +0800

    initial check-in

---
+++ StreamingInSignalR/Hubs/StreamHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace StreamingInSignalR.Hubs
+{
+    public class StreamHub : Hub
+    {
+        public ChannelReader<int> DelayCounter(int delay)
+        {
+            var channel = Channel.CreateUnbounded<int>();
+
+            _ = WriteItems(channel.Writer, 20, delay);
+
+            return channel.Reader;
+        }
+
+        private async Task WriteItems(ChannelWriter<int> writer, int count, int delay)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                await writer.WriteAsync(i);
+                await Task.Delay(delay);
+            }
+
+            writer.TryComplete();
+        }
+    }
+}
